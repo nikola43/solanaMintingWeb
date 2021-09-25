b667b823fe2742e031e915e0bfba3f38379d6a40
paulo <paulo.soares@wiserentr.com>
2021-09-25 22:09:06 +0200

    ok

--- src/Home.tsx
+++ src/Home.tsx
@@ -475,7 +475,188 @@ const Home = (props: HomeProps) => {
               </div>
             </section>
 
-            <Collection />
+            <section>
+              <div id="" className="container">
+                <div className="row">
+
+                  <img loading="lazy" src="img/banner2.png" alt="header image" />
+
+                </div>
+                <div className="row" id="story">
+                  <div className="col-md-12 col-sm-12 col-xs-12">
+                    <h3>
+                      The Story
+                    </h3>
+                  </div>
+                  <div className="col-md-12 col-sm-12 col-xs-12 transparent">
+                    <p>
+                      BabyPunks are the spawn of what happens when you have a bunch of badass CryptoPunks mixed
+                      with pop culture, internet memes and peyote. This lot are looking for a new home and are up
+                      for adoption. They were originally going to find new homes on Ethereum, but it was too old,
+                      congested and full of other babies who looked neglected. The BabyPunks banded together and
+                      set a course for uncharted territory - Solana. It’s every pixel's dream and now they have
+                      their chance at the big leagues.
+                    </p>
+                    <p>
+                      All they need is the right person to take them under their wing and teach them the ropes.
+                      These punks may be small but they have big dreams and their properties uniquely reflect who
+                      they are and promise to reward their owners handsomely.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section id="roadmap">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h3 className="roadmap-headline">Roadmap</h3>
+                  </div>
+                </div>
+              </div>
+              <div className="container">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>20% Sold</h4>
+                          <h6 className="red">NFT Giveaway</h6>
+                          <p>A random airdrop of 20 NFTs to early community members in our discord.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>40% Sold</h4>
+                          <h6 className="red">BabyPunks 10x Baby Bonus</h6>
+                          <p>On the 4000th sale, there will be a payout 10x the mint price in SOL to 4
+                            community members in our discord.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>50% Sold</h4>
+                          <h6 className="red">BabyPunks Merch</h6>
+                          <p>BabyPunks Merch store will be rolled out where NFT holders will get first dibs
+                            and exclusive access to limited edition t-shirts, hats, and hoodies. Non-NFT
+                            holders will be given access at a later date.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>60% Sold</h4>
+                          <h6 className="red">BabyPunks Sweeper</h6>
+                          <p>A liquidity wallet will be created that will be used to buy the floor of BabyPunk
+                            NFTs.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>70% Sold</h4>
+                          <h6 className="red">BabyPunks Gives Back</h6>
+                          <p>The community will direct a $10,000 gift to a charity of the community's
+                            choosing.
+                            A grant of $15,000 will be set up to fund a community project that contributes
+                            to the growth of BabyPunks.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>80% Sold</h4>
+                          <h6 className="red">TeenPunks</h6>
+                          <p>We will create a limited collection of stylized TeenPunks and airdrop them to all
+                            holders.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="road-content">
+                        <div className="red-process"></div>
+                        <div className="road-detail">
+                          <h4>100% Sold</h4>
+                          <h6 className="red">The PUNK-A-TRON Arcade</h6>
+                          <p>We will build an even bigger arcade than we planned for previously that will host
+                            approximately 10 titles by the end of this year. The games will feature retro
+                            old school classics. All NFT holders will be able to stake their NFTs to earn
+                            BabyPunk Tokens from transaction fees paid by players to play the games to
+                            compete head to head for prizes such popular NFTs, and SOL tokens. </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <br />
+            <br />
+            <br />
+            <section>
+              <div id="team" className="container text-center">
+                <div className="row">
+                  <div className="col-lg-12 col-sm-12 col-xs-12">
+                    <h3>
+                      The Team
+                    </h3>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-lg-3 col-sm-6 col-xs-12">
+                    <img src="assets/baby_3.png" alt="dev" />
+                    <p>
+                      BabyPunkGuy
+                    </p>
+                    <p>
+                      Dev
+                    </p>
+                  </div>
+                  <div className="col-lg-3 col-sm-6 col-xs-12">
+                    <img src="./img/baby_5.png" alt="smmark" />
+                    <p>
+                      Nefu
+                    </p>
+                    <p>
+                      Social Media Marketing
+                    </p>
+                  </div>
+                  <div className="col-lg-3 col-sm-6 col-xs-12">
+                    <img src="./img/baby_7.png" alt="man" />
+                    <p>
+                      SK
+                    </p>
+                    <p>
+                      Project Management
+                    </p>
+                  </div>
+                  <div className="col-lg-3 col-sm-6 col-xs-12">
+                    <img src="./img/baby_8.png" alt="marks" />
+                    <p>
+                      CaptainM00n
+                    </p>
+                    <p>
+                      Marketing Strategy
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
           </div>
         </>
         )
